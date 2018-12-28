5266308e3d2c1957d5952d4c0eb86ae485af035d
sabinbogati1 <sabin.bogati@studentpartner.com>
2018-12-28 16:04:36 +0545

    Jwt in backend..

--- server/routes/api.js
+++ server/routes/api.js
@@ -1,4 +1,5 @@
 const express = require("express");
+const jwt = require("jsonwebtoken");
 const router = express.Router();
 const User = require("../model/user");
 
@@ -27,7 +28,9 @@ router.post("/register", (req, res) => {
             console.log(error);
         }
         else {
-            res.status(200).send(registeredUser);
+            let payload = { subject: registeredUser._id };
+            let token = jwt.sign(payload, 'secretKey');
+            res.status(200).send({token});
         }
     })
 });
@@ -47,7 +50,9 @@ router.post("/login", (req, res) => {
                 if (user.password !== userData.password) {
                     res.status(401).send("Invalid Password")
                 } else {
-                    res.status(200).send(user);
+                    let payload = { subject: user._id };
+                    let token = jwt.sign(payload, 'secretKey');
+                    res.status(200).send({token});
                 }
             }
         }
