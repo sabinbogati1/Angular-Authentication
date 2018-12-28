7d10462c5657937b6ae24db7cc139d2c56821738
sabinbogati1 <sabin.bogati@studentpartner.com>
2018-12-28 15:20:34 +0545

    Added login part

--- ngApp/src/app/app.module.ts
+++ ngApp/src/app/app.module.ts
@@ -2,11 +2,13 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import {FormsModule } from "@angular/forms";
 import { AppRoutingModule } from './app-routing.module';
+import {HttpClientModule} from "@angular/common/http";
 import { AppComponent } from './app.component';
 import { RegisterComponent } from './register/register.component';
 import { LoginComponent } from './login/login.component';
 import { EventsComponent } from './events/events.component';
 import { SpecialEventsComponent } from './special-events/special-events.component';
+import { AuthService} from "./auth.service";
 
 @NgModule({
   declarations: [
@@ -19,9 +21,10 @@ import { SpecialEventsComponent } from './special-events/special-events.componen
   imports: [
     BrowserModule,
     AppRoutingModule,
-    FormsModule
+    FormsModule,
+    HttpClientModule
   ],
-  providers: [],
+  providers: [AuthService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
--- ngApp/src/app/register/register.component.ts
+++ ngApp/src/app/register/register.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import {AuthService} from "../auth.service";
 
 @Component({
   selector: 'app-register',
@@ -8,13 +9,19 @@ import { Component, OnInit } from '@angular/core';
 export class RegisterComponent implements OnInit {
 
   registerUserData = {};
-  constructor() { }
+  constructor(private _auth:AuthService) {  }
 
   ngOnInit() {
   }
 
   registerUser() {
-    console.log("this.registerUserData === ", this.registerUserData)
+   // console.log("this.registerUserData === ", this.registerUserData)
+
+    this._auth.registerUser(this.registerUserData).subscribe(
+      res => console.log(res),
+      err => console.log(err)
+    )
+
   }
 
 }
