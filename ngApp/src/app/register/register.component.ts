e88371704d608f06a14c3e348022a5c4b1215391
sabinbogati1 <sabin.bogati@studentpartner.com>
2018-12-28 14:54:58 +0545

    Registration Ui complete..

--- ngApp/src/app/register/register.component.ts
+++ ngApp/src/app/register/register.component.ts
@@ -7,9 +7,14 @@ import { Component, OnInit } from '@angular/core';
 })
 export class RegisterComponent implements OnInit {
 
+  registerUserData = {};
   constructor() { }
 
   ngOnInit() {
+  }
+
+  registerUser() {
+    console.log("this.registerUserData === ", this.registerUserData)
   }
 
 }
